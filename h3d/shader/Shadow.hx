fed739c44162d17adc955004ccf8538e7ec435c8
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2023-07-09 23:09:34 +0530

    make shadow work

--- h3d/shader/Shadow.hx
+++ h3d/shader/Shadow.hx
@@ -13,10 +13,13 @@ class Shadow extends hxsl.Shader {
 		var pixelColor : Vec4;
 		var transformedPosition : Vec3;
 		var pixelTransformedPosition : Vec3;
-		@private var shadowPos : Vec3;
+		@var var shadowPos : Vec3;
 
+		function vertex() {
+			shadowPos = transformedPosition * shadow.proj;
+		}
 		function fragment() {
-			var shadowPos = pixelTransformedPosition * shadow.proj;
+			
 			var depth = shadow.map.get(screenToUv(shadowPos.xy));
 			var zMax = shadowPos.z.saturate();
 			var delta = (depth + shadow.bias).min(zMax) - zMax;
